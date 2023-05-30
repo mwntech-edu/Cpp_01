885726ec98edb16aa55f7ec2cda738b1d669e5aa
mwntech-edu <mwntechnology@gmail.com>
2023-05-30 16:22:19 +0900

    default parameter order

--- Cpp_01/DefaultParameter.cpp
+++ Cpp_01/DefaultParameter.cpp
@@ -9,6 +9,10 @@ int MyFuncTwo(int num1 = 5, int num2 = 7) {
 	return num1 + num2;
 }
 
+int YourFunc(int num1, int num2 = 20, int num3 = 30) {
+	return num1 + num2 + num3;
+}
+
 int main() {
 
 	std::cout << "MyFuncOne with no parameter: " << MyFuncOne() << std::endl;
@@ -17,6 +21,9 @@ int main() {
 	std::cout << "MyFuncTwo with no parameters: " << MyFuncTwo() << std::endl;
 	std::cout << "MyFuncTwo with parameters 5, 7: " << MyFuncTwo(5, 7) << std::endl;
 	std::cout << "MyFuncTwo with parameters 2, 3: " << MyFuncTwo(2, 3) << std::endl;
+	std::cout << "YourFunc : " << YourFunc(10) << std::endl;
+	std::cout << "YourFunc : " << YourFunc(10, 11) << std::endl;
+	std::cout << "YourFunc : " << YourFunc(10, 11, 13) << std::endl;
 	return 0;
 }
 
