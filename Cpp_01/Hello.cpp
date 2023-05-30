92da0d4c3c29cc7b477ab537a9ddd0004e72e5db
mwntech-edu <mwntechnology@gmail.com>
2023-05-30 14:46:54 +0900

    초기 커밋.

---
+++ Cpp_01/Hello.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(void)
+{
+	int num = 20;
+
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello " << "World!" << std::endl;
+	std::cout << num << ' ' << 'A';
+	std::cout << ' ' << 3.14 << std::endl;
+
+	int val1;
+	std::cout << "첫 번째 숫자입력: ";
+	std::cin >> val1;
+
+	int val2;
+	std::cout << "두 번째 숫자입력: ";
+	std::cin >> val2;
+
+	int result = val1 + val2;
+	std::cout << "덧셈결과: " << result << std::endl;
+
+	return 0;
+}
