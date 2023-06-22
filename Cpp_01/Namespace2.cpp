a0ca9b2eb7141fabfb010fc58398a22026a8cae7
mwntech-edu <mwntechnology@gmail.com>
2023-06-22 10:36:53 +0900

    namespace 2

---
+++ Cpp_01/Namespace2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+namespace Pusan {
+	namespace Aban {
+		int chulsu = 170;
+	}
+
+	namespace Bban {
+		int chulsu = 171;
+	}
+}
+
+namespace Seoul {
+	namespace Aban {
+		int chulsu = 175;
+	}
+
+	namespace Bban {
+		int chulsu = 176;
+		void print() {
+			std::cout << "서울고 A반 철수 : " << Aban::chulsu << std::endl;
+			std::cout << "서울고 B반 철수 : " << chulsu << std::endl;
+			std::cout << "부산고 A반 철수 : " << Pusan::Aban::chulsu << std::endl;
+			std::cout << "부산고 B반 철수 : " << Pusan::Bban::chulsu << std::endl;
+		}
+	}
+}
+int main() {
+	Seoul::Bban::print();
+	return 0;
+}
