b046ce4e4c0cf87878198a57aab35855795225d2
mwntech-edu <mwntechnology@gmail.com>
2023-06-22 13:51:29 +0900

    ptr

--- Cpp_01/Namespace3.cpp
+++ Cpp_01/Namespace3.cpp
@@ -8,7 +8,23 @@ int val = 100;
 int main() {
 
 	int val = 20;
+	int* intPtr = &val;
+	int** intPPtr = &intPtr;
+	bool testTrue = true;
+	bool testFalse = false;
+	bool* boolPtr = &testTrue;
+	bool* boolPtr2 = &testFalse;
+
 	cout << "Hello World!! Local val: " <<  val << " Global val: " << ::val << endl;
+	
+	cout << "True : " << testTrue << " False : " << testFalse << endl;
+
+	cout << "size of 1 " << sizeof(1) << " byte " << endl;
+	cout << "size of int " << sizeof(int) << " byte " << endl;
+	cout << "size of true " << sizeof(true) << " byte " << endl;
+	cout << "size of bool " << sizeof(bool) << " byte " << endl;
+	cout << "size of long " << sizeof(long) << " byte " << endl;
+	cout << "size of pointer " << sizeof(int*) << " byte " << endl;
 
 	return 0;
 }
