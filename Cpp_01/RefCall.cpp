c3c1f2711a66ad7afe710e001f2b027cafa6472a
mwntech-edu <mwntechnology@gmail.com>
2023-06-22 16:17:46 +0900

    return ref error

--- Cpp_01/RefCall.cpp
+++ Cpp_01/RefCall.cpp
@@ -1,16 +1,30 @@
 #include<iostream>
 using namespace std;
-
 int& RefRetFuncOne(int& ref) {
 	return ++ref;
 }
-
+int RefRetFuncTwo(int& ref) {
+	return ++ref;
+}
+int& RetRefFunc(int n) {
+	int num = 20;
+	num += n;
+	return num;
+}
 int main() {
 	int num1 = 1;
 	//int& num2 = RefRetFuncOne(num1);
-	int num2 = RefRetFuncOne(num1);
+	//int num2 = RefRetFuncOne(num1);
+	int num2 = RefRetFuncTwo(num1);
+	//int& num2 = RefRetFuncTwo(num1); // (X)
 	num1 += 1;
 	num2 += 100;
 	cout << num1 << " : " << num2 << endl;
+
+	int& refRet = RetRefFunc(10);
+	//num2 = RefRetFuncTwo(num1);
+	//refRet++;
+	cout << refRet << endl;
+	cout << refRet << endl;
 	return 0;
 }
