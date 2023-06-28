f48a86fbeb412815921ddf9de46efbea39974d23
mwntech-edu <mwntechnology@gmail.com>
2023-06-28 10:25:37 +0900

    heap array delete

--- Cpp_01/NewAndDelete.cpp
+++ Cpp_01/NewAndDelete.cpp
@@ -6,11 +6,26 @@ int main() {
 	int& ref = *ptr;
 	int* (&refptr) = ptr;
 	ref = 20;
+
+	int* arr1 = new int[3];
+	double* arr2 = new double[7];
+	*arr1 = 2;
+	*(arr1 + 1) = 4;
+	*(&arr1[1] + 1) = 8;
+	for (int i = 0; i < 7; i++) arr2[i] = i;
+	
 	cout << *ptr << endl;
 	cout << ref << endl;
 	cout << *refptr << endl;
-
 	cout << refptr << endl;
 	cout << &ref << endl;
+	cout << "sizeof arr1 : " << sizeof(*arr1) << endl;
+	for (int i = 0; i < 3; i++)
+		cout << arr1[i] << endl;
+	cout << endl;
+	for (int i = 0; i < 7; i++) cout << arr2[i] << endl;
+	delete ptr;
+	delete[] arr1;
+	delete[] arr2;
 	return 0;
 }
