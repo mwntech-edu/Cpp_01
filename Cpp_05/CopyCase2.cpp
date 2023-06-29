cb516b8052279f46041ea42ffe00840bf6ee49b4
mwntech-edu <mwntechnology@gmail.com>
2023-06-29 15:36:19 +0900

    temp obj

--- Cpp_05/CopyCase2.cpp
+++ Cpp_05/CopyCase2.cpp
@@ -10,21 +10,36 @@ public:
 	SoSimple(SoSimple& copy) : num1(copy.num1), num2(copy.num2) {
 		cout << "Called SoSimple(SoSimple& copy)" << endl;
 	}
+	SoSimple& AddNum(int n) {
+		num1 += n;
+		return *this;
+	}
 	void ShowSimpleData() {
 		cout << num1 << endl;
 		cout << num2 << endl;
 	}
 };
 
-void SimpleFuncObj(SoSimple ob) {
+SoSimple SimpleFuncObj(SoSimple ob) {
 	ob.ShowSimpleData();
+	cout << " return 이전 " << endl;
+	return ob;
 }
 
 int main() {
+	// Case2
 	SoSimple obj(7, 8);
 	cout << "함수 호출 전" << endl;
 	SimpleFuncObj(obj);	// 복사 생성자가 호출됨
 	cout << "함수 호출 후" << endl;
+
+	// Case3
+	SoSimple obj2(7, 10);
+	SimpleFuncObj(obj2).AddNum(30).ShowSimpleData();
+	//ob.AddNum(30);
+	//ob.ShowSimpleData();
+
+	obj2.ShowSimpleData();
 	return 0;
 }
 
