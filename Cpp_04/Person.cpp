ff98e8d209a450ac055708159eed9ee971b86db6
mwntech-edu <mwntechnology@gmail.com>
2023-06-29 11:52:01 +0900

    object pointer array

--- Cpp_04/Person.cpp
+++ Cpp_04/Person.cpp
@@ -6,6 +6,10 @@ private:
 	char* name;
 	int age;
 public:
+	Person() {
+		name = NULL;
+		age = 0;
+	}
 	Person(char const* myname, int myage) {
 		int len = strlen(myname) + 1;		// chulsu = 5 + 1
 		name = new char[len];
@@ -22,8 +26,24 @@ public:
 	}
 };
 
+
+
 int main() {
 	Person chul("철수", 20);
 	chul.ShowPersonInfo();
+
+	Person arr[3];	// Person 3개
+	Person* parr = new Person[3]; // 포인터 1개 + Person 3개
+
+	Person* arrP[3]; // 포인터 3개
+	arrP[0] = new Person("Chulsu", 20);
+	arrP[1] = &chul;
+	arrP[2] = new Person("영희", 23);
+
+	for (int i = 0; i < sizeof(arrP) / sizeof(arrP[0]); i++)
+	{
+		arrP[i]->ShowPersonInfo();
+	}
+
 	return 0;
 }
